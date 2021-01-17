861d0013196b9cb1ac10a2b2614ac1f255c093d9
kemal-erdi <63252357+kemal-erdi@users.noreply.github.com>
2021-01-17 18:36:25 +0300

    Add project files.

---
+++ SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) 
+        {
+            Console.WriteLine("Sepete Eklendi" + urun.Adi);
+            
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, int id)
+        {
+            Console.WriteLine("Sepete Eklendi");
+
+        }
+    }
+}
